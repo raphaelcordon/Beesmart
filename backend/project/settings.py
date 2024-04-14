223c3ada03199310c162dd0afc3526bd15c9178c
Vytas <vytas.urbelis@gmail.com>
2024-04-14 18:06:38 +0200

    First commit with first files

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['team2.propulsion-learn.ch']
+ALLOWED_HOSTS = ['besmart.propulsion-learn.ch']
 
 if DEBUG is True:
     ALLOWED_HOSTS += ['*']
