cee6030be4e0bfefc738aedfa408a04cefc2e581
Vytas <vytas.urbelis@gmail.com>
2024-04-14 18:10:39 +0200

    First commit with first files

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django API",
+        default_version='v1',
+        description="Description of your Django App",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="academy@constructor.org"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,  # Set to False restrict access to protected endpoints
+    permission_classes=(permissions.AllowAny,),  # Permissions for docs access
+)
 
 urlpatterns = [
     path('backend/api/admin/', admin.site.urls),
+    path('backend/api/auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/api/auth/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
+    path('backend/api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
