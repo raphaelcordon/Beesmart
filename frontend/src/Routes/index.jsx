6ce614d22d56b8815e9e29d161ce42189ad47b9a
sejlana <sejlanashop@gmail.com>
2024-04-17 11:42:29 +0200

    saving progress registerbusiness

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -5,6 +5,7 @@ import NotFound from "./NotFound";
 import SignUpEndUser from "./StartingPagesRoutes/SignUpEndUser";
 import BusinessSignUp from "./StartingPagesRoutes/BusinessSignUp";
 import Login from "./StartingPagesRoutes/Login";
+import CongratulationsSection from "./StartingPagesRoutes/SignUpStepsBusiness/CongratulationSection";
 
 const Router = () => {
   return (
@@ -15,6 +16,7 @@ const Router = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/private-signup" element={<SignUpEndUser />} />
           <Route path="/business-signup" element={<BusinessSignUp />} />
+          <Route path="/congratulations" element={<CongratulationsSection />} />
 
           <Route path="*" element={<NotFound />} />
         </Route>
