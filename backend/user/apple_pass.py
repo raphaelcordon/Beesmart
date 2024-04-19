07bd572678c478ecf1f2e9628f996c8e634a2d6c
Vytas <vytas.urbelis@gmail.com>
2024-04-19 14:13:43 +0200

    fixed secret_key in qr_code

--- backend/user/apple_pass.py
+++ backend/user/apple_pass.py
@@ -1,28 +1,28 @@
-from pkpass import PKPass
-
-def create_wallet_pass(request):
-    # Define your pass data
-    pass_data = {
-        "passTypeIdentifier": "pass.com.example",
-        "teamIdentifier": "AB123CD456",
-        "organizationName": "Example Corp",
-        "serialNumber": "123456",
-        "logoText": "Example Logo",
-        "description": "Example Pass",
-        "foregroundColor": "rgb(255, 255, 255)",
-        "backgroundColor": "rgb(107, 156, 196)",
-        # Add more fields as required by your pass type
-    }
-
-    # Load your certificates and key
-    certificate = '/path/to/certificate.pem'
-    key = '/path/to/key.pem'
-    apple_wwdr_certificate = '/path/to/AppleWWDRCA.pem'
-    password = 'certificate_password'  # If your key is password protected
-
-    # Create and sign the pass
-    pkpass = PKPass(pass_data)
-    pkpass.sign(passfile, key, apple_wwdr_certificate, password, manifest_required=True)
-    response = HttpResponse(pkpass, content_type='application/vnd.apple.pkpass')
-    response['Content-Disposition'] = 'attachment; filename="pass.pkpass"'
-    return response
+# from pkpass import PKPass
+#
+# def create_wallet_pass(request):
+#     # Define your pass data
+#     pass_data = {
+#         "passTypeIdentifier": "pass.com.example",
+#         "teamIdentifier": "AB123CD456",
+#         "organizationName": "Example Corp",
+#         "serialNumber": "123456",
+#         "logoText": "Example Logo",
+#         "description": "Example Pass",
+#         "foregroundColor": "rgb(255, 255, 255)",
+#         "backgroundColor": "rgb(107, 156, 196)",
+#         # Add more fields as required by your pass type
+#     }
+#
+#     # Load your certificates and key
+#     certificate = '/path/to/certificate.pem'
+#     key = '/path/to/key.pem'
+#     apple_wwdr_certificate = '/path/to/AppleWWDRCA.pem'
+#     password = 'certificate_password'  # If your key is password protected
+#
+#     # Create and sign the pass
+#     pkpass = PKPass(pass_data)
+#     pkpass.sign(passfile, key, apple_wwdr_certificate, password, manifest_required=True)
+#     response = HttpResponse(pkpass, content_type='application/vnd.apple.pkpass')
+#     response['Content-Disposition'] = 'attachment; filename="pass.pkpass"'
+#     return response
