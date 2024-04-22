848dc4d22645c020c37b271c3b4e7725f736162c
raphaelcordon <cordon@outlook.com>
2024-04-22 16:07:34 +0200

    rebuilding UX for Auth/Reg

--- frontend/src/axios/axiosEndUser.js
+++ frontend/src/axios/axiosEndUser.js
@@ -22,11 +22,9 @@ export const GetMeUser = async () => {
     }
 }
 
-export const GetEndUserVerify = async (secretkey) => {
+export const PostEndUserVerify = async (userData) => {
     try{
-        const res = await axios.get(`/enduser/user/verify/${secretkey}`,{
-            responseType: 'blob', // Handles the response as a binary file
-          });
+        const res = await axios.post(`/enduser/user/verify/${userData.secret_key}`, userData);
         return res.data;
     } catch (error) {
         throw error("Not possible to fetch data");
