a2b592faf03f230a24e366ea5c113d65ed25b15e
sejlana <sejlanashop@gmail.com>
2024-04-23 11:59:46 +0200

    styles with fetched pictures

--- frontend/src/Components/BusinessUserComponents/QrCode.jsx
+++ frontend/src/Components/BusinessUserComponents/QrCode.jsx
@@ -2,8 +2,8 @@ import { Document, Page, Text, View, StyleSheet, Image, PDFViewer, PDFDownloadLi
 import backgroundImageUrl from "../../assets/loyaltyprogram_default_background.png"
 import Button from '../SmallComponents/Button';
 import { useSelector } from 'react-redux';
-import beelogo from "../../../public/beeicon.png";
-import qrcodeexample from "../../assets/qrcodeexample.png";
+// import beelogo from "../../../public/beeicon.png";
+// import qrcodeexample from "../../assets/qrcodeexample.png";
 
 
 // Styles for PDF
@@ -70,15 +70,15 @@ const styles = StyleSheet.create({
 
 import PropTypes from 'prop-types';
 
-const TextToPDF = ({ website, qrCode, logo, businessName }) => (
+const TextToPDF = ({ website, qrCode, logo}) => (
   <Document>
     <Page size="A4" style={{ ...styles.page }}>
       <View style={styles.pageBackgroundContainer}>
         <Image src={backgroundImageUrl} style={styles.imageBackground} />
       </View>
-      {/* <Image src={qrCode} style={styles.styledqrcode} /> */}
+      <Image src={qrCode} style={styles.styledqrcode} />
       
-      <Image src={qrcodeexample} style={styles.styledqrcode} />
+      {/* <Image src={qrcodeexample} style={styles.styledqrcode} /> */}
       <Text style={styles.headingPrimary}>
       Terms& Conditions:
         </Text>
@@ -86,8 +86,8 @@ const TextToPDF = ({ website, qrCode, logo, businessName }) => (
         {website}
       </Text>
       
-      {/* <Image src={logo} style={styles.image} /> */}
-      <Image src={beelogo} style={styles.image} />
+      <Image src={logo} style={styles.image} />
+      {/* <Image src={beelogo} style={styles.image} /> */}
     </Page>
   </Document>
 );
@@ -96,7 +96,7 @@ TextToPDF.propTypes = {
   website: PropTypes.string.isRequired,
   qrCode: PropTypes.string.isRequired, 
   logo: PropTypes.string.isRequired,
-    businessName: PropTypes.string.isRequired,
+    //businessName: PropTypes.string.isRequired,
 };
 
 const QrCode = () => {
@@ -104,10 +104,10 @@ const QrCode = () => {
   const website = CustomerUser.customer_user_profile.website;
   const qrcode = CustomerUser.customer_user_profile.qr_code;
   const logo = CustomerUser.customer_user_profile.logo;
-  const businessName = CustomerUser.customer_user_profile.business_name;
+ // const businessName = CustomerUser.customer_user_profile.business_name;
  //const qrcode = localStorage.getItem('qrCode'); // Retrieve QR code from local storage
 
-  const pdfData = <TextToPDF website={website} qrCode={qrcode} businessName={businessName} logo={logo} />; 
+  const pdfData = <TextToPDF website={website} qrCode={qrcode} logo={logo} />; 
 
   return (
     <div className="flex items-center justify-center h-full">
