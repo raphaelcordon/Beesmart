414a1e5356a2a1821bf83d9eb51bc1ae249ccdb8
sejlana <sejlanashop@gmail.com>
2024-04-23 14:32:15 +0200

    styling navbar homepage, installed fontawesome

--- frontend/src/Components/NavBarHome.jsx
+++ frontend/src/Components/NavBarHome.jsx
@@ -1,22 +1,36 @@
 import beeLogo from "../../public/beeicon.png";
 import { Link } from "react-router-dom";
 import NavBarLink from "./SmallComponents/NavBarLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faRocket, faUser} from "@fortawesome/free-solid-svg-icons";
 
 export const NavBarHome = () => {
   return (
     <div className="navbar">
+      
       <div className="navbar-start">
         <div>
           <Link to="/">
-            <img src={beeLogo} className="h-10 w-10 sm:h-10 sm:w-10 lg:h-20 lg:w-20" alt="Bee Logo" />
-            <p className="text-l">BeeSmart</p>
+            <img src={beeLogo}  className="h-20 sm:h-10 lg:h-20 " alt="Bee Logo" />
+            <p className="text-xl">
+  <span className="font-bold">B</span>
+  <span className="font-semibold">ee</span><span className="font-bold">S</span><span className="font-semibold">mart</span>
+</p>
           </Link>
         </div>
       </div>
+      {/* <div className="navbar-end"> */}
+      <div className="navbar-end flex py-4"> 
+  <div><NavBarLink to="/get-started">
+    <FontAwesomeIcon icon={faRocket} />
+    <span>Get</span> <span>Started</span>
+  </NavBarLink></div>
+  <div className="ml-10 mr-5"> <NavBarLink to="/login">
+    <FontAwesomeIcon icon={faUser}/>
+    <span> LogIn</span>
+  </NavBarLink></div>
 
-      <div className="navbar-center flex flex-wrap justify-center md:justify-between items-center py-4">
-        <NavBarLink to="/get-started">Get Started</NavBarLink>
-        <NavBarLink to="/login">LogIn</NavBarLink>
+        
       </div>
     </div>
   );
