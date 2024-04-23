5a560293d0f0c232aaf9114888bd600257b7f997
Vytas <vytas.urbelis@gmail.com>
2024-04-23 16:06:02 +0200

    Adding collector type to campaign model

--- backend/collector/admin.py
+++ backend/collector/admin.py
@@ -6,6 +6,3 @@ from .models import Collector
 @admin.register(Collector)
 class CollectorStyleAdmin(admin.ModelAdmin):
     list_display = ['value_counted', 'date_created', 'is_collected', 'campaign', 'end_user_profile']
-
-
-
