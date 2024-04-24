97dcdf61425b4e6cfcfd30824fad54366bebd057
sejlana <sejlanashop@gmail.com>
2024-04-24 17:11:05 +0200

    navbar for endusers

--- frontend/src/Components/EndUserComponents/NavBarUser.jsx
+++ frontend/src/Components/EndUserComponents/NavBarUser.jsx
@@ -1,17 +1,17 @@
-import {Link, useNavigate} from "react-router-dom";
-import beeLogo from "../../../public/beeicon.png";
+import {useNavigate} from "react-router-dom";
+//import beeLogo from "../../../public/beeicon.png";
 import NavBarToggling from "../SmallComponents/NavBarToggling.jsx";
 import { useEffect, useRef, useState } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {logoutUserEndUser} from "../../store/slices/userEndUserSlice.js";
 import useGetMeEndUser from "../../Hooks/useGetMeEndUser.js";
 import NavBarLink from "../SmallComponents/NavBarLinks.jsx";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faBullhorn, faExpand, faRightFromBracket, faTicketSimple, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const NavBarBusiness = ({ setActiveTabProp }) => {
-    const [isOpen, setIsOpen] = useState(false);
+    //const [isOpen, setIsOpen] = useState(false);
     const [activeTab, setActiveTab] = useState('Scan');
     const menuRef = useRef(null);
     const dispatch = useDispatch();
@@ -58,6 +58,21 @@ const NavBarBusiness = ({ setActiveTabProp }) => {
 
 
     return (
+
+
+<>
+ {/* Logout button for mobile users */}
+ <div className="absolute top-3 right-3 p-2 sm:hidden text-3xl">
+ <NavBarLink to="/" onClick={(e) => {
+           e.preventDefault();
+           logoutHandler();
+           navigate("/");
+         }}><FontAwesomeIcon icon={faRightFromBracket} />
+     </NavBarLink>
+     </div>
+
+
+
         <div className="navbar border border-base-300 bg-base-100/50 shadow-lg backdrop-blur-2xl fixed bottom-0 left-0 w-full z-10 md:relative md:flex md:justify-between sm:justify-around">
             {/* <div className="navbar-start"> */}
                 {/* <div>
@@ -79,19 +94,23 @@ const NavBarBusiness = ({ setActiveTabProp }) => {
 
             <div ref={menuRef}
                  className="navbar-start">
-                    <div className="flex justify-between">
-                    <div className="ml-0 sm:ml-2">
+                    {/* <div className="flex justify-between"> */}
+                    {/* <div className="ml-0 sm:ml-2"> */}
 
                 <NavBarToggling setActiveTab={() => handleSetActiveTab('MyCampaigns')} active={activeTab === 'MyCampaigns'}
-                                tabName="MyCampaigns">My Campaigns</NavBarToggling>
-                                </div>
+                                tabName="MyCampaigns"><FontAwesomeIcon icon={faBullhorn} />Campaigns</NavBarToggling>
+                                <div  className="mx-3" >
                 <NavBarToggling setActiveTab={() => handleSetActiveTab('MyVouchers')} active={activeTab === 'MyVouchers'}
-                                tabName="MyVouchers">My Vouchers</NavBarToggling>
+                                tabName="MyVouchers">
+                                    <FontAwesomeIcon icon={faTicketSimple} />Vouchers</NavBarToggling></div></div>
+                                    <div className="navbar-end md:navbar-center">
+                <div  className="block lg:hidden mx-6" >
                 <NavBarToggling setActiveTab={() => handleSetActiveTab('Scan')}
                                 active={activeTab === 'Scan'}
-                                tabName="Scan">Scan</NavBarToggling>
+                                tabName="Scan"><FontAwesomeIcon icon={faExpand} />Scan</NavBarToggling></div>
+                                <div  className="mx-2" >
                 <NavBarToggling setActiveTab={() => handleSetActiveTab('Profile')} active={activeTab === 'Profile'}
-                                tabName="Profile">Profile</NavBarToggling>
+                                tabName="Profile"><FontAwesomeIcon icon={faUser} />Profile</NavBarToggling></div></div>
                 <div className="hidden sm:inline-block">
                 <NavBarLink to="/" onClick={(e) => {
                       e.preventDefault();
@@ -104,8 +123,10 @@ const NavBarBusiness = ({ setActiveTabProp }) => {
                 </NavBarLink>
                 </div>
                 </div>
-            </div>
-        </div>
+                {/* </div> */}
+            
+        {/* </div> */}
+        </>
     );
 }
 
