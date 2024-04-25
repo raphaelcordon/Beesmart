16f980fa66218326e202c233aa3e0e16972ca03c
sejlana <sejlanashop@gmail.com>
2024-04-25 13:16:11 +0200

    navbars wrapped in div

--- frontend/src/Components/BusinessUserComponents/NavBarBusiness.jsx
+++ frontend/src/Components/BusinessUserComponents/NavBarBusiness.jsx
@@ -35,67 +35,69 @@ const NavBarBusiness = ({ setActiveTabProp }) => {
     };
 
     return (
-        <> {/* Logout button for mobile users */}
-        <div className="absolute top-3 right-3 p-2 sm:hidden text-3xl">
-            <NavLink to="/" onClick={(e) => {
-                      e.preventDefault();
-                      logoutHandler();
-                      navigate("/");
-                    }}><FontAwesomeIcon icon={faRightFromBracket} />
+        <>
+            {/* Logout button for mobile users */}
+            <div className="absolute top-3 right-3 p-2 sm:hidden text-3xl">
+                <NavLink to="/" onClick={(e) => {
+                    e.preventDefault();
+                    logoutHandler();
+                    navigate("/");
+                }}>
+                    <FontAwesomeIcon icon={faRightFromBracket} />
                 </NavLink>
-        </div>
-        <div className="fixed md:hidden bottom-12 left-1/2 transform -translate-x-1/2 z-50" >
-            <NavBarTogglingNewMobile setActiveTab={() => handleSetActiveTab('NewCampaign')}
-                                active={activeTab === 'NewCampaign'}
-                                tabName="NewCampaign">
-                                    <FontAwesomeIcon icon={faCirclePlus}  style={{ fontSize: "2.5em" }} /></NavBarTogglingNewMobile></div>
-
-        <div className="navbar border border-base-300 bg-base-100/50 shadow-lg backdrop-blur-2xl fixed bottom-0 left-0 w-full z-10 md:relative md:flex md:justify-between sm:justify-around">
-
-
-            <div ref={menuRef}
-             className="navbar-start">
-
-
-                <NavBarToggling setActiveTab={() => handleSetActiveTab('MyCampaigns')}
-                                active={activeTab === 'MyCampaigns'}
-                                tabName="MyCampaigns">
-                                    <FontAwesomeIcon icon={faBell} />Campaigns</NavBarToggling>
-                                    
-                <NavBarToggling setActiveTab={() => handleSetActiveTab('Insights')} active={activeTab === 'Insights'}
-                                tabName="Insights">
-                                    <FontAwesomeIcon icon={faChartLine} />Insights</NavBarToggling>
-                                    </div>
-                                    <div className="navbar-end md:navbar-center">
-                                    <div  className="mx-6" > 
-                <NavBarToggling setActiveTab={() => handleSetActiveTab('QrCode')} active={activeTab === 'QrCode'}
-                                tabName="QrCode">
-                                    <FontAwesomeIcon icon={faQrcode} />QR</NavBarToggling></div>
-                                    
-                                    
-                                    <NavBarTogglingNew
-    setActiveTab={() => handleSetActiveTab('NewCampaign')}
-    active={activeTab === 'NewCampaign'}
-    tabName="NewCampaign"
->
-    <FontAwesomeIcon icon={faCirclePlus} /> New 
-    <div className="block">Campaign</div>
-</NavBarTogglingNew>
-                                    
-                                    <div  className="mx-3" >
-                <NavBarToggling setActiveTab={() => handleSetActiveTab('Settings')} active={activeTab === 'Settings'}
-                                tabName="Settings">
-                                    <FontAwesomeIcon icon={faScrewdriverWrench} />Settings</NavBarToggling>
-                                    </div></div>
-                                 
-                <div className="hidden sm:inline-block">
-                <NavBarLink to="/" onClick={(e) => {
-                      e.preventDefault();
-                      logoutHandler();
-                      navigate("/");
-                    }}><FontAwesomeIcon icon={faRightFromBracket} /><span> Logout</span>
-                </NavBarLink></div>
             </div>
+            <div className="fixed md:hidden bottom-12 left-1/2 transform -translate-x-1/2 z-50">
+                <NavBarTogglingNewMobile setActiveTab={() => handleSetActiveTab('NewCampaign')}
+                                           active={activeTab === 'NewCampaign'}
+                                           tabName="NewCampaign">
+                    <FontAwesomeIcon icon={faCirclePlus} style={{ fontSize: "2.5em" }} />
+                </NavBarTogglingNewMobile>
+            </div>
+
+            <div className="navbar border border-base-300 bg-base-100/50 shadow-lg backdrop-blur-2xl fixed bottom-0 left-0 w-full z-10 md:relative md:flex md:justify-between sm:justify-around">
+
+                <div ref={menuRef} className="navbar-start flex justify-around ">
+                <div className="w-13" >
+                    <NavBarToggling setActiveTab={() => handleSetActiveTab('MyCampaigns')}
+                                   active={activeTab === 'MyCampaigns'}
+                                   tabName="MyCampaigns">
+                        <FontAwesomeIcon icon={faBell} />Campaigns
+                    </NavBarToggling></div>
+                    <div className="w-13" >
+                    <NavBarToggling setActiveTab={() => handleSetActiveTab('Insights')}
+                                   active={activeTab === 'Insights'}
+                                   tabName="Insights">
+                        <FontAwesomeIcon icon={faChartLine} />Insights
+                    </NavBarToggling></div>
+                </div>
+
+                <div className="navbar-end flex justify-around">
+                <div className="w-13" >
+                    <NavBarToggling style={{ marginLeft: '10px' }} setActiveTab={() => handleSetActiveTab('QrCode')}
+                                   active={activeTab === 'QrCode'}
+                                   tabName="QrCode" className="">
+                        <FontAwesomeIcon icon={faQrcode} />QR
+                    </NavBarToggling></div>
+                    <div className="w-13" >
+                    <NavBarToggling setActiveTab={() => handleSetActiveTab('Settings')}
+                                   active={activeTab === 'Settings'}
+                                   tabName="Settings">
+                        <FontAwesomeIcon icon={faScrewdriverWrench} />Settings
+                    </NavBarToggling></div>
+                
+                </div>
+
+                <div className="hidden sm:inline-block">
+                    <NavBarLink to="/" onClick={(e) => {
+                        e.preventDefault();
+                        logoutHandler();
+                        navigate("/");
+                    }}>
+                        <FontAwesomeIcon icon={faRightFromBracket} /><span> Logout</span>
+                    </NavBarLink>
+                </div>
+                </div>
+            
         </>
     );
 }
