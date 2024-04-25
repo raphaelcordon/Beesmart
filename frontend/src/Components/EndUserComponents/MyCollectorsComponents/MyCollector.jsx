ccb5211df01815111469ec7f5380d0afaf2f6bce
Vytas <vytas.urbelis@gmail.com>
2024-04-25 13:18:27 +0200

    working on collectors

--- frontend/src/Components/EndUserComponents/MyCollectorsComponents/MyCollector.jsx
+++ frontend/src/Components/EndUserComponents/MyCollectorsComponents/MyCollector.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { getCollectorByEndUser } from "../../../axios/axiosCollector";
 import stamp from "../../../assets/stamp.png";
 import notcollected from "../../../assets/notcollected.png";
+import defaultlogo from "../../../assets/defaultlogo.png"
+import check from "../../../assets/check.png"
 
 const MyCollector = ({ item }) => {
   const [collector, setCollector] = useState({});
@@ -24,23 +26,29 @@ const MyCollector = ({ item }) => {
   return (
     <>
       {loading ? (
-        <div className="flex justify-center pb-10">
-          <div className="rounded-md bg-zinc-100 flex flex-col text-primary-content breadcrumbs w-80 shadow-md">
+        <div className="flex justify-center pb-20">
+          <div className="rounded-md bg-zinc-50 flex flex-col text-primary-content breadcrumbs w-80 shadow-md">
             <div className="pt-64 pb-64">.....</div>
           </div>
         </div>
       ) : (
-        <div key={item.id || index} className="flex justify-center w-full mb-10">
-          <div className="rounded-md bg-zinc-100 flex flex-col text-primary-content shadow-md">
-            <div className="stat flex">
-              <img src={`${item.logo}`} alt="Campaign Logo" className="w-12 h-12 rounded-full p-2" />
-              <div className="text-3xl font-bold text-yellow-500">{item.name}</div>
+        <div key={item.id || index} className="flex justify-center w-full pb-10 mb-10">
+          <div className="rounded-md bg-zinc-50 flex flex-col text-primary-content w-80 shadow-md">
+            <div className="stat flex items-center">
+              <img src={`${item.logo || defaultlogo}`} alt="Campaign Logo" className="w-16 h-16 rounded-full p-2" />
+              <div className="text-l text-left font-bold  text-lime-700">{item.name.toUpperCase()}</div>
             </div>
             <div>
-              <div className="stat-title w-full text-right pr-14">
-                {collectorType === 1 ? <>Your Stamps: {collector.value_counted} </> : <></>}
+              <div className="stat-title w-full ">
+                {collectorType === 1 ? (
+                  <div className="text-right pr-14">Your Stamps: {collector.value_counted} </div>
+                ) : collectorType === 2 ? (
+                  <div className="text-xl font-bold">You need: {collector.value_goal} points</div>
+                ) : (
+                    <div className="text-xl font-bold">Campaign goal: {collector.value_goal} CHF</div>
+                )}
               </div>
-              <div className="flex items-center justify-center h-40">
+              <div className="flex items-center justify-center min-h-24">
                 {console.log(item)}
 
                 <div className="flex flex-row flex-wrap items-center justify-center rounded-lg w-80">
@@ -48,14 +56,10 @@ const MyCollector = ({ item }) => {
                     Array.from({ length: collector.value_goal }).map((_, index) =>
                       collector.value_counted > index ? (
                         <div
-                          className="m-1 bg-zinc-300 border-2 border-slate-400 rounded-full w-12 h-12 flex items-center justify-center"
+                          className="m-1 bg-zinc-300  border-slate-400 rounded-full w-12 h-12 flex items-center justify-center"
                           key={index}
                         >
-                          <img
-                            src={stamp}
-                            alt="Stamp"
-                            className="opacity-75 grayscale w-11 h-11 rounded-full"
-                          />
+                          <img src={check} alt="Stamp" className="opacity-50 w-11 h-11 rounded-full" />
                         </div>
                       ) : (
                         <div
@@ -65,9 +69,15 @@ const MyCollector = ({ item }) => {
                       ),
                     )
                   ) : collectorType === 2 ? (
-                    <div className="text-slate-200 text-2xl">Your points: {collector.value_counted}</div>
+                    <div>
+                      <div className="text-slate-400 text-xl">You have:</div>
+                      <div className="text-slate-600 font-bold text-4xl">{collector.value_counted}</div>
+                    </div>
                   ) : (
-                    <div className="text-slate-200 text-2xl">You already spent: {collector.value_counted}CHF</div>
+                    <div>
+                      <div className="text-slate-400 text-sm">You already spent:</div>
+                      <div className="text-slate-600 font-bold text-2xl"> {collector.value_counted}CHF</div>
+                    </div>
                   )}
                 </div>
               </div>
@@ -78,7 +88,9 @@ const MyCollector = ({ item }) => {
                   className="opacity-25 grayscale w-52 h-52"
                 />
               </div>
-              <div className="text-xs stat-title w-full text-right pr-14 pb-4">Campaign expires: {item.ending_date ? <div>{item.ending_date}</div> : <div className="text-xs stat-title w-full text-right pr-14 pb-4">No expiration date</div>}</div>
+              <div className="text-xs stat-title w-full text-right pr-14 pb-4">
+                <div className="text-yellow-500">Campaign expires:</div>  {item.ending_date ? <div>{item.ending_date}</div> : <div>---</div>}
+              </div>
               <div className="stat-desc"></div>
             </div>
           </div>
