1aa68e485bc8be9e9d560aa3d16b8c72f6af9991
raphaelcordon <cordon@outlook.com>
2024-04-25 14:32:15 +0200

    Bug fixing, login end user fixing

--- frontend/src/Routes/ProtectedRoutes/index.jsx
+++ frontend/src/Routes/ProtectedRoutes/index.jsx
@@ -7,7 +7,7 @@ const ProtectedRoutes = () => {
     const isCustomerLoggedIn = useSelector((state) => state.customer.accessToken)
     const isUserLoggedIn = useSelector((state) => state.endUser.accessToken)
 
-    return !isCustomerLoggedIn || !isUserLoggedIn ? (
+    return !isCustomerLoggedIn && !isUserLoggedIn ? (
       // Navigate to the /login route and replace the current history entry
       // replace true will replace the current history entry instead of adding a new one
       // this prevents users from an infinite loop of redirects when clicking the back button
